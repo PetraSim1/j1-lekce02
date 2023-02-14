f6071f7fd6df5f393f1cfca8ceda962518692bf3
Petra Simanska <simanska.petra@gmail.com>
2023-02-14 20:21:29 +0100

    domeček a prasátko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,7 +7,73 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
+        nakresliDOmecek();
+
+        zofka.turnRight(135);
+        zofka.penUp();
+        zofka.move(600);
+        zofka.penDown();
+
+
+        zofka.turnRight(180);
+
+        nakresliPrasatko();
+
+
+    }
+
+    private void nakresliPrasatko() {
+        nakresliDOmecek();
+        zofka.turnRight(45);
+        zofka.move(200);
+        nakresliZdaniNohu();
+        zofka.turnRight(135);
+        zofka.penUp();
+        zofka.move(200);
+        nakresliPredniNohu();
+    }
+
+    private void nakresliPredniNohu() {
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.turnLeft(90);
+        zofka.move(50);
+        zofka.turnLeft(180);
+        zofka.move(50);
+    }
+
+    private void nakresliZdaniNohu() {
+        zofka.turnLeft(45);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka. penUp();
+        zofka.move(50);
+        zofka.penDown();
+        zofka.turnRight(90);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+    }
+
+    private void nakresliDOmecek() {
+        nakresliCtverec();
+        zofka.penUp();
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(143);
+        zofka.turnRight(90);
+        zofka.move(143);
+    }
+
+    private void nakresliCtverec() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move (200);
+            zofka.turnRight(90);
+        }
     }
 
     public static void main(String[] args) {
